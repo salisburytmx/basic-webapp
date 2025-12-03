09b402bd6ddcaa7fb158b6318bf6e859f3925255
Mike Salisbury <michael.salisbury@trimedx.com>
2025-12-03 15:57:56 -0800

    feat: add api

---
+++ api/src/functions/message.js
@@ -0,0 +1,17 @@
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+        const greeting = "Hello";
+        if (name == 'mike') { greeting = "Howdy" }
+        if (name == 'joe') { greeting = "Yo" }
+        if (name == 'mel') { greeting = "Sup" }
+
+        return { body: JSON.stringify({ "text": "${greeting} ${name}, from the API!" }) };
+    }
+});
